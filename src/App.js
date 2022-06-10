1786828acd458ef685bf895ce870f1bc4960fb67
gagandeepkatodia <gagandeepkatodia@gmail.com>
2022-06-10 10:22:29 +0200

    Commit before pull

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import DetailsPage from "./pages/DetailsPage/index.js";
 import ShopPage from "./pages/ShopPage/index.js";
 import HomePage from "./pages/HomePage/index.js";
 import NavBar from "./components/NavBar/index.js";
+import Banner from "./components/HeroBanner/index.js";
 import "./components/NavBar/styles.css";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
       <h1>Welcome to my webshop</h1>
       <Routes>
         <Route path="/" element={<HomePage />} />
---
+++ src/components/HeroBanner/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+const HeroBanner = () => {
+  return (
+    <div className="banner">
+      <p>Banner</p>
+      <h3>Gagan</h3>
+    </div>
+  );
+};
+
+export default HeroBanner;
